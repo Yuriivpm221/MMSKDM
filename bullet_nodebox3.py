a25c2b0103dfc96ac834081bd0687dfc2a64cfb2
Yuriivpm221 <yurii.ivankiv-pm221@nung.edu.ua>
2024-11-28 21:32:34 +0200

    Update bullet_nodebox3.py

--- bullet_nodebox3.py
+++ bullet_nodebox3.py
@@ -9,42 +9,35 @@ def f(xp, yp, x1p, y1p):
     return x, y, x1, y1
 
 def draw(canvas):
-    global x, y, x1, y1, t, dt, xt, s, x1t
+    global x, y, x1, y1, t, xt, s, x1t
     if canvas.mouse.button == LEFT:
         t, x, y, x1, y1 = 0, 0, 0, canvas.mouse.x, canvas.mouse.y
-    if x1 != 0 and y1 != 0 and y >= 0:
+    
+    if x1 and y1 and y >= 0:
         x, y, x1, y1 = f(x, y, x1, y1)
         t += dt
-
+    
     canvas.clear()
 
-    # Малюємо снаряд
     fill(0, 0, 0)
     ellipse(x=x, y=y, width=10, height=10)
 
-    # Малюємо ціль
     fill(1, 0, 0)
     rect(x=xt, y=0, width=30, height=20)
 
-    # Перевірка на влучання
     if xt < x < xt + 30 and 0 < y < 20:
         s += 1
         x, y, x1, y1 = 0, 0, 0, 0
-        xt = 700
-        x1t -= 0.1
+        xt, x1t = 700, max(x1t - 0.1, -1)
 
-    # Закінчення гри, якщо ціль вийшла за межі
     if xt < 0:
         exit()
 
-    # Відображення рахунку
     fill(0, 0, 0)
     text(f"Score: {s}", 300, 300)
 
-    # Рух цілі
     xt += x1t
 
-# Ініціалізація змінних
 x, y, x1, y1 = 0, 0, 0, 0
 dt = 0.1
 t = 0
